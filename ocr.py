c0f09c945f8cbf622eef895678507bd227ad9f48
varun <varunshrivastav36@gmail.com>
2023-08-20 12:05:42 +0530

    first commit

--- ocr.py
+++ ocr.py
@@ -4,32 +4,12 @@ import sys
 
 
 async def read_image(img_path, lang='eng'):
-    """
-    Performs OCR on a single image
-
-    :img_path: str, path to the image file
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :text: str, converted text from image
-    """
-
     try:
         return pytesseract.image_to_string(img_path, lang=lang)
     except:
         return "[ERROR] Unable to process file: {0}".format(img_path)
 
 async def read_images_from_dir(dir_path, lang='eng', write_to_file=False):
-    """
-    Performs OCR on all images present in a directory
-
-    :dir_path: str, path to the directory of images
-    :lang: str, language to be used while conversion (optional, default is english)
-
-    Returns
-    :converted_text: dict, mapping of filename to converted text for each image
-    """
-
     converted_text = {}
     for file_ in os.listdir(dir_path):
         if file_.endswith(('png', 'jpeg', 'jpg')):
@@ -41,9 +21,6 @@ async def read_images_from_dir(dir_path, lang='eng', write_to_file=False):
     return converted_text
 
 def _write_to_file(text, file_path):
-    """
-    Helper method to write text to a file
-    """
     print("[INFO] Writing text to file: {0}".format(file_path))
     with open(file_path, 'w') as fp:
         fp.write(text)
